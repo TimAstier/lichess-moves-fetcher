b08329fb4fb53e1be25d5a98453331419a0f5032
Timothée Astier <timothee.astier@essec.edu>
2021-11-27 12:39:40 +0100

    Find all *KEY* moves in a study

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 
 const { flat } = require("chess-moments");
+// https://github.com/victorocna/chess-moments
 
 const headers = {
   Authorization: "Bearer " + process.env.lichessToken,
@@ -13,16 +14,25 @@ const ONE_STUDY_URL = "https://lichess.org/api/study/RVw9HG6u.pgn";
 fetch(ONE_STUDY_URL, { headers }).then((res) => {
   res.text().then((pgn) => {
     const pgns = pgn.split(/(\[Event)/);
-    // console.log(pgns);
-    const firstPgn = `${pgns[1]}${pgns[2]}`;
-    const moments = flat(firstPgn);
-    const filteredMoments = moments.filter((m) => {
-      if (m.comment === undefined) {
-        return false;
-      }
-      return m.comment.includes("*KEY*");
+    // TODO: Make this dynamic
+    const firstChapter = `${pgns[1]}${pgns[2]}`;
+    const secondChapter = `${pgns[3]}${pgns[4]}`;
+    const thirdChapter = `${pgns[5]}${pgns[6]}`;
+
+    const positions = [];
+
+    [firstChapter, secondChapter, thirdChapter].forEach((chapter) => {
+      const moments = flat(chapter);
+      const filteredMoments = moments.filter((m) => {
+        // TODO: Augment with previous position
+        if (m.comment === undefined) {
+          return false;
+        }
+        return m.comment.includes("*KEY*");
+      });
+      positions.push(filteredMoments);
     });
-    console.log(filteredMoments);
+    console.log(positions);
     console.log("done");
   });
 });
